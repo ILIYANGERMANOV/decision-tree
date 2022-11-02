7afb56e8c0b58e14b8b0cddd906b7756e1ea877c
Iliyan Germanov <iliyan.germanov971@gmail.com>
2022-11-02 23:13:34 +0200

    Initialize project following best practices from "Now In Android"

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("ivyapps.android.application")
+    id("ivyapps.android.application.compose")
+    id("ivyapps.android.hilt")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.ivyapps.decision"
+
+    compileSdk = 33
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+    implementation(libs.kotlinx.datetime)
+
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.material.iconsExtended)
+    implementation(libs.androidx.compose.material3)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.ui.util)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.runtime.livedata)
+    implementation(libs.androidx.metrics)
+    implementation(libs.androidx.tracing.ktx)
+    implementation(libs.androidx.activity.compose)
+}
